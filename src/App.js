97e8891fab0552fc1005464b752113baf533b13b
qxan <mehmet.enfa@gmail.com>
2022-12-14 21:43:13 +0300

    img slider

--- src/App.js
+++ src/App.js
@@ -1,46 +1,57 @@
-import {useRef, useEffect} from "react"
+import { clear } from "@testing-library/user-event/dist/clear";
+import { useRef, useEffect } from "react";
 
 function App() {
-
-  const ref = useRef()
+  const ref = useRef();
 
   useEffect(() => {
-    let images = ref.current.querySelectorAll('img'),
-      total = images.lenght,
-      current = 0
-    setInterval(() => {
-      images[current].classList.remove('opacity-0')
-      if (current === total - 1) {
-        current = 0
+    let images = ref.current.querySelectorAll("img"),
+      total = images.length,
+      current = 0;
+    const imageSlider = () => {
+      if (current > 0) {
+        images[current - 1].classList.add("opacity-0");
       } else {
-        current += 1
+        images[total - 1].classList.add("opacity-0");
       }
-    }, 2000)
-  
-  }, [ref])
-  
+
+      images[current].classList.remove("opacity-0");
+      if (current === total - 1) {
+        current = 0;
+      } else {
+        current += 1;
+      }
+    }
+    let interval = setInterval(imageSlider, 3000);
+    return () => {
+      clearInterval(interval)
+    }
+  }, [ref]);
 
   return (
     <div className="h-full w-full flex  items-center justify-center">
       <div className="w-[380px] h-[581px] bg-logo-pattern relative bg-[lenght:468.32px ,634.15px] bg-[top_left_-46px]">
-        <div className="w-[250px] h-[538px] absolute top-[27px] right-[18px]" ref={ref}>
+        <div
+          className="w-[250px] h-[538px] absolute top-[27px] right-[18px]"
+          ref={ref}
+        >
           <img
-            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity"
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
             src="https://www.instagram.com/static/images/homepage/screenshots/screenshot1-2x.png/cfd999368de3.png"
             alt=""
           />
           <img
-            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity"
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
             src="https://www.instagram.com/static/images/homepage/screenshots/screenshot2-2x.png/80b8aebdea57.png"
             alt=""
           />
           <img
-            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity"
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
             src="https://www.instagram.com/static/images/homepage/screenshots/screenshot3-2x.png/fe2540684ab2.png"
             alt=""
           />
           <img
-            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity "
+            className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear"
             src="https://www.instagram.com/static/images/homepage/screenshots/screenshot4-2x.png/8e9224a71939.png"
             alt=""
           />
