fc4d614e8d4ae42242dd7030c405a21d29181a13
qxan <mehmet.enfa@gmail.com>
2022-12-15 23:01:12 +0300

    login page ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useRef, useEffect, useState } from "react";
 import Input from "./components/input";
-import {AiFillFacebook} from 'react-icons/ai'
+import { AiFillFacebook } from "react-icons/ai";
 
 function App() {
   const ref = useRef();
@@ -36,8 +36,8 @@ function App() {
   }, [ref]);
 
   return (
-    <div className="h-full w-full flex items-center gap-x-8 justify-center">
-      <div className="w-[380px] h-[581px] bg-logo-pattern relative bg-[lenght:468.32px ,634.15px] bg-[top_left_-46px]">
+    <div className="h-full w-full flex flex-wrap overflow-auto items-center gap-x-8 justify-center">
+      <div className="hidden md:block w-[380px] h-[581px] bg-logo-pattern relative bg-[lenght:468.32px ,634.15px] bg-[top_left_-46px]">
         <div
           className="w-[250px] h-[538px] absolute top-[27px] right-[18px]"
           ref={ref}
@@ -65,46 +65,61 @@ function App() {
         </div>
       </div>
 
-      <div className="w-[350px] bg-white border px-[40px] pt-10 pb-2">
-        <a href="#" className="flex justify-center mb-8">
-          <img
-            className="h-[51px]"
-            src="https://www.instagram.com/static/images/web/logged_out_wordmark-2x.png/d2529dbef8ed.png"
-            alt=""
-          />
-        </a>
-        <form className="grid gap-y-1.5">
-          <Input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            label="Phone Number, username or email"
-          />
-          <Input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            label="Password"
-          />
-          <button
-            type="submit"
-            disabled={!enabled}
-            className="h-[30px] rounded bg-brand font-medium text-white text-sm disabled:opacity-50"
-          >
-            Log In
-          </button>
-          <div className="flex items-center">
-            <div className="h-px bg-gray-300 flex-1" />
-            <span className="px-4 text-[13px] text-gray-500 font-semibold">
-              OR
-            </span>
-            <div className="h-px bg-gray-300 flex-1" />
-          </div>
-          <a href="#" className="flex justify-center items-center gap-x-2 text-sm font-semibold text-facebook">
+      <div className="w-[350px] grid gap-y-3">
+        <div className="bg-white border px-[40px] pt-10 pb-6">
+          <a href="#" className="flex justify-center mb-8">
+            <img
+              className="h-[51px]"
+              src="https://www.instagram.com/static/images/web/logged_out_wordmark-2x.png/d2529dbef8ed.png"
+              alt=""
+            />
+          </a>
+          <form className="grid gap-y-1.5">
+            <Input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              label="Phone Number, username or email"
+            />
+            <Input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              label="Password"
+            />
+            <button
+              type="submit"
+              disabled={!enabled}
+              className="h-[30px] mt-1 rounded bg-brand font-medium text-white text-sm disabled:opacity-50"
+            >
+              Log In
+            </button>
+            <div className="flex items-center my-2.5 mb-3.5">
+              <div className="h-px bg-gray-300 flex-1" />
+              <span className="px-4 text-[13px] text-gray-500 font-semibold">
+                OR
+              </span>
+              <div className="h-px bg-gray-300 flex-1" />
+            </div>
+            <a
+              href="#"
+              className="flex justify-center mb-2.5 items-center gap-x-2 text-sm font-semibold text-facebook"
+            >
               <AiFillFacebook size={20} />
               Log in with Facebook
-          </a>
-        </form>
+            </a>
+            <a
+              href="#"
+              className="text-xs flex items-center justify-center text-link"
+            >
+              Forgot password?
+            </a>
+          </form>
+        </div>
+
+        <div className="bg-white border p-4 text-sm text-center">
+          Don't have an account? <a href="#" className="font-semibold text-brand">Sign up</a>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       colors: {
         brand: '#0095f6',
         facebook: '#385185',
+        link: '#00376b',
       },
       backgroundImage: {
         'logo-pattern': 'url(https://static.cdninstagram.com/rsrc.php/v3/y4/r/ItTndlZM2n2.png)'
