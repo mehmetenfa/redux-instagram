6f50c8babceea6d54ed844e2d16eb0faa4df2517
qxan <mehmet.enfa@gmail.com>
2022-12-19 19:35:50 +0300

    firebase install

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCUGW21PawzVoy3W_jnn3dUFKPqS_MM-0k",
+  authDomain: "redux-instagram.firebaseapp.com",
+  projectId: "redux-instagram",
+  storageBucket: "redux-instagram.appspot.com",
+  messagingSenderId: "59243538153",
+  appId: "1:59243538153:web:c7fe1d05aced718962f278"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
