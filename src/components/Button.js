b167fe18879c923d7eff224872a3e1205db86a96
qxan <mehmet.enfa@gmail.com>
2023-01-17 22:32:06 +0300

    props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,11 @@
-export default function Button() {
+export default function Button({type = 'button', children, ...props}) {
   return (
     <button
-      type="submit"
-      disabled={!isValid || !dirty || isSubmitting}
+      type={type}
+      {...props}
       className="h-[30px] mt-1 rounded bg-brand font-medium text-white text-sm disabled:opacity-50"
     >
-      Log In
+      {children}
     </button>
   );
 }
